c2feff81a69615018946908442e5aaed4ed26b49
JJEllacopulos <jj.ellacopulos@gmail.com>
2020-09-17 20:03:05 -0300

    Inicio

---
+++ src/Pack/Principal.java
@@ -0,0 +1,22 @@
+package Pack;
+
+import java.awt.EventQueue;
+
+public class Principal {
+	
+	Ventana frame;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ventana frame = new Ventana();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+}
